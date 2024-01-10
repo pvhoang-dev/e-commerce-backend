cadb11f4037a3681b718db53c3183ea5b7390510
bino <hoangpham23052003@gmail.com>
2024-01-10 23:49:49 +0700

    Create request banners

---
+++ app/Http/Requests/Admin/CreateBannerRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use App\Models\Banner;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+
+class CreateBannerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'slug' => Str::slug($this->title),
+            'status' => 1,
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return Banner::getValidationRules();
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return Banner::messages();
+    }
+}
