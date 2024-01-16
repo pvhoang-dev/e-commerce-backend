2ca70c39659f4f92bfe83968cb2d70ddbaa015bb
bino <hoangpham23052003@gmail.com>
2024-01-16 15:11:06 +0700

    Update feature: delete category

---
+++ resources/views/admin/menus/create.blade.php
@@ -0,0 +1,141 @@
+@extends('admin.layouts.master')
+
+@section('title')
+    Add Category
+
+    <a href="{{ route('admin.categories.index') }}" class="btn btn-outline-info float-right">
+        <i class="uil uil-corner-up-left-alt"></i> Back
+    </a>
+@endsection
+@section('content')
+    <div class="card">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="{{ route('admin.categories.store') }}" method="POST">
+            @csrf
+            <div class="card-body">
+                <div class="row">
+                    <div class="form-group col-6">
+                        <label for="cate_name">Name</label>
+                        <input type="text" name="name" id="cate_name" class="form-control" value="{{ old('name') }}">
+                        @if ($errors->has('name'))
+                            <span class="text-danger">{{ $errors->first('name') }}</span>
+                        @endif
+                    </div>
+                    <div class="form-group col-6">
+                        <label for="cate_po">Position</label>
+                        <input type="number" name="position" id="cate_po" class="form-control" value="{{ old('position') }}">
+                        @if ($errors->has('position'))
+                            <span class="text-danger">{{ $errors->first('position') }}</span>
+                        @endif
+                    </div>
+
+                    <div class="form-group col-12">
+                        <label for="cate_parent">Category Parent</label>
+                        <select class="form-control select2" name="parent_id" id="cate_parent" data-toggle="select2">
+                            <option value="0">Category Parent</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}" {{ old('parent_id') == $category->id ? 'selected' : ''}}>{{ $category->name }}</option>
+                            @endforeach
+                        </select>
+                        @if ($errors->has('parent_id'))
+                            <span class="text-danger">{{ $errors->first('parent_id') }}</span>
+                        @endif
+                    </div>
+
+                    <div class="form-group col-12">
+                        <div class="dropzone">
+                            <div class="dz-message needsclick">
+                                <label for="uploadFile" style="cursor: pointer; display:block;">
+                                    <i class="h1 text-muted dripicons-cloud-upload"></i>
+                                    <h4>Drop files here or click to upload.</h4>
+                                    <input type="file" id="uploadFile" style="display: none">
+                                </label>
+                            </div>
+                        </div>
+                        <!-- Preview -->
+                        <div class="dropzone-previews mt-3">
+                            <div class="card mt-1 mb-0 shadow-none border dz-processing dz-error dz-complete">
+                            </div>
+                        </div>
+                        @if ($errors->has('file_id'))
+                            <span class="text-danger">{{ $errors->first('file_id') }}</span>
+                        @endif
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-footer float-right border-0 bg-transparent">
+                <button type="submit" class="btn btn-primary" id="create">Create</button>
+            </div>
+        </form>
+    </div>
+@endsection
+@push('js')
+    <script>
+        $('#uploadFile').change(function () {
+            let formData = new FormData();
+
+            let files = $(this)[0].files;
+
+            for (let i = 0; i < files.length; i++) {
+                formData.append('files[]', files[i]);
+            }
+
+            let response = uploadFile(formData);
+            console.log(response);
+
+            if (response.status === 'success') {
+                let files = response.data.files;
+
+                let html = '';
+
+                const listImages = $('.dropzone-previews .card');
+
+                listImages.empty();
+
+                for (let i = 0; i < files.length; i++) {
+                    var file = files[i];
+
+                    let urlImage = "{{ route('file.draft.show', ['file_id' => 'file_id']) }}";
+                    urlImage = urlImage.replace('file_id', file.id);
+
+                    let urlDeleteImage = "{{ route('file.draft.delete', ['file_id' => 'file_id']) }}";
+                    urlDeleteImage = urlDeleteImage.replace('file_id', file.id);
+                    html += `<div class="p-2">
+                                    <div class="row align-items-center">
+                                        <input type="hidden" name="file_id" value="${file.id}" />
+                                        <div class="col-auto">
+                                            <img src="${urlImage}" class="avatar-sm rounded bg-light">
+                                        </div>
+                                        <div class="col pl-0">
+                                            <a href="javascript:void(0);" class="text-muted font-weight-bold">
+                                                ${file.name}
+                                                </a>
+                                        </div>
+                                        <div class="col-auto">
+
+                                            <a href="${urlDeleteImage}" class="btn btn-link btn-lg text-muted" data-dz-remove="">
+                                                <i class="dripicons-cross"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>`;
+                }
+
+                listImages.append(html);
+
+
+            } else {
+                alert('Upload ảnh lỗi');
+            }
+        });
+    </script>
+@endpush
---
+++ resources/views/admin/menus/index.blade.php
@@ -0,0 +1,72 @@
+@extends('admin.layouts.master')
+
+@section('title')
+    List Category
+
+    <a href="{{ route('admin.categories.create') }}" class="btn btn-outline-info float-right">
+        Add Category
+    </a>
+@endsection
+@section('content')
+    <div class="card">
+        <div class="card-body p-0">
+            <div class="table-responsive users-table">
+                @if ($categories->count())
+                    <table class="table table-centered mb-0 " id="attributes-table">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Image</th>
+                            <th>Position</th>
+                            <th>Parent</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($categories as $category)
+                            <tr>
+                                <td>{{ $category->name }}</td>
+                                <td style="max-width: 20px">
+                                    @if ($category->file_id)
+                                        <img class="rounded mr-1 mb-3 mb-sm-0 img-fluid"
+                                             src="{{route('file.show', $category->file_id)}}" alt="">
+                                    @else
+                                        Chưa có ảnh
+                                    @endif
+                                </td>
+                                <td>{{ $category->position }}</td>
+                                <td>{{ $category -> parent_id != 0 ? $category->parentCategory -> name : 'Parent'}}</td>
+                                <td>
+                                    <form action="{{ route('admin.categories.delete', $category->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <div class='btn-group'>
+                                            <a class="action-icon"
+                                               href="{{ route('admin.categories.edit', ['id' => $category->id]) }}">
+                                                <i class="mdi mdi-pencil"></i>
+                                            </a>
+                                            <button type="submit" class="action-icon delete border-0 bg-transparent"
+                                                    onclick="return confirm('Are you sure?')">
+                                                <i class="mdi mdi-delete"></i>
+                                            </button>
+                                        </div>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
+@push('js')
+    <script>
+        // Blade syntax to check if the 'error' session variable is present
+        @if(session('error'))
+        // Display an alert with the error message
+        alert("{{ session('error') }}");
+        @endif
+    </script>
+@endpush
