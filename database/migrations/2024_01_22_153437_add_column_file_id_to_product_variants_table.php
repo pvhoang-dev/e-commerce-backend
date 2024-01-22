22ab0ba030c9b661521c949883bbf614f366c767
bino <hoangpham23052003@gmail.com>
2024-01-23 00:04:36 +0700

    Feature: create & edit variant image 2 (add column)

---
+++ database/migrations/2024_01_22_153437_add_column_file_id_to_product_variants_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('product_variants', function (Blueprint $table) {
+            $table->integer('file_id')->after('status')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('product_variants', function (Blueprint $table) {
+            $table->dropColumn('file_id');
+        });
+    }
+};
